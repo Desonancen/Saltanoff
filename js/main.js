e58dc9d205bbcf0f57da085e116e436edcfcebcb
valer13572@gmail.com <46524230+Desonancen@users.noreply.github.com>
2020-03-25 18:28:32 +0200

    form

--- js/main.js
+++ js/main.js
@@ -29,3 +29,29 @@ function showSlides(n) {
     //dots[slideIndex - 1].className = "active";
 }
 
+const selectSingle = document.querySelector('.select__form');
+const selectSingle_title = selectSingle.querySelector('.form__item-select');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+
+// Reset title
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', () => {
+  selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
+});
