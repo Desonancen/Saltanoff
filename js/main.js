4b5099f4e12f3642181887b0a644695716055981
iva_bogdan <iva_bogdan@ukr.net>
2020-03-25 13:59:18 +0200

    js

--- js/main.js
+++ js/main.js
@@ -16,12 +16,16 @@ function showSlides(n) {
         slideIndex = slides.length
     }
     for (i=0;i < slides.length; i++) {
-        slides[i].style.display = "none";
+       slides[i].style.display = "none";
     }
     for (i=0; i< dots.length; i++) {
+
         dots[i].className = dots[i].className.replace("active","");
     }
+
+    dots[n-1].classList.add("active");
+   
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "active";
+    //dots[slideIndex - 1].className = "active";
 }
 
