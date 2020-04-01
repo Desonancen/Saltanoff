238d790495e9b771d256dda9edf92af0a35c6903
iva_bogdan <iva_bogdan@ukr.net>
2020-04-01 17:37:22 +0300

    122

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var slideIndex=1;
+ //do something
+   var slideIndex=1;
 showSlides(slideIndex);
 
 function currentSlide(n) {
@@ -29,16 +30,22 @@ function showSlides(n) {
     //dots[slideIndex - 1].className = "active";
 }
 
-const selectSingle = document.querySelector('.select__form');
-const selectSingle_title = selectSingle.querySelector('.form__item-select');
-const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+//
+//('#0').addEventListener('click', () => {
+//  a = 0;
+//});
+
+var selectSingle = document.querySelectorAll('.select__form');
+var selectSingle_title = selectSingle.querySelector('.form__item-select');
+var selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
 
 // Toggle menu
 selectSingle_title.addEventListener('click', () => {
   if ('active' === selectSingle.getAttribute('data-state')) {
-    selectSingle.setAttribute('data-state', '');
+    selectSingle[a].setAttribute('data-state', '');
   } else {
-    selectSingle.setAttribute('data-state', 'active');
+    selectSingle[a].setAttribute('data-state', 'active');
   }
 });
 
@@ -51,7 +58,8 @@ for (let i = 0; i < selectSingle_labels.length; i++) {
 }
 
 // Reset title
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', () => {
-  selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
-});
+//const reset = document.querySelector('.reset');
+//reset.addEventListener('click', (evt) => {
+//  selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
+//});
+
