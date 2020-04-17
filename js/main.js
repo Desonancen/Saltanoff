089df82cfe2b71cb91ebdfd10835582b686defae
BogdanIvaniychuk <49246867+BogdanIvaniychuk@users.noreply.github.com>
2020-04-18 00:57:27 +0300

    fixed in js

--- js/main.js
+++ js/main.js
@@ -29,32 +29,74 @@ function showSlides(n) {
     //dots[slideIndex - 1].className = "active";
 }
 
-const selectSingle = document.querySelector('.select__form');
-const selectSingle_title = selectSingle.querySelector('.form__item-select');
-const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
 
-// Toggle menu
-selectSingle_title.addEventListener('click', () => {
-  if ('active' === selectSingle.getAttribute('data-state')) {
-    selectSingle.setAttribute('data-state', '');
-  } else {
-    selectSingle.setAttribute('data-state', 'active');
-  }
-});
 
-// Close when click to option
-for (let i = 0; i < selectSingle_labels.length; i++) {
-  selectSingle_labels[i].addEventListener('click', (evt) => {
-    selectSingle_title.textContent = evt.target.textContent;
-    selectSingle.setAttribute('data-state', '');
+let select_form = ["#town", "#type_of_property", "#price_for", "#price_to", "#square", "#bedroom_for", "#bedroom_to"];
+
+
+
+function findClick(){
+  const selectSingle = [document.querySelector(select_form[0]), document.querySelector(select_form[1]), document.querySelector(select_form[2]), document.querySelector(select_form[3]), document.querySelector(select_form[4]), document.querySelector(select_form[5]), document.querySelector(select_form[6])]
+  const selectSingle_title = [selectSingle[0].querySelector('.form__item-select'), selectSingle[1].querySelector('.form__item-select'), selectSingle[2].querySelector('.form__item-select'), selectSingle[3].querySelector('.form__item-select'), selectSingle[4].querySelector('.form__item-select'), selectSingle[5].querySelector('.form__item-select'), selectSingle[6].querySelector('.form__item-select')]
+
+  selectSingle_title[0].addEventListener('click', () => {
+    SelectForm(0);
+  });
+
+  selectSingle_title[1].addEventListener('click', () => {
+    SelectForm(1);
+  });
+
+  selectSingle_title[2].addEventListener('click', () => {
+    SelectForm(2);
+  });
+
+  selectSingle_title[3].addEventListener('click', () => {
+    SelectForm(3);
+  });
+
+  selectSingle_title[4].addEventListener('click', () => {
+    SelectForm(4);
+  });
+
+  selectSingle_title[5].addEventListener('click', () => {
+    SelectForm(5);
+  });
+
+  selectSingle_title[6].addEventListener('click', () => {
+    SelectForm(6);
   });
 }
 
 
-// Reset title
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', () => {
-  selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
-});
+function SelectForm(i) {
+  const selectSingle = document.querySelector(select_form[i]);
+  const selectSingle_title = selectSingle.querySelector('.form__item-select');
+  const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+  
+  // Toggle menu   
+  if ('active' === selectSingle.getAttribute('data-state')) {
+  selectSingle.setAttribute('data-state', '');
+  } else {
+  selectSingle.setAttribute('data-state', 'active');
+  }
+  
+  // Close when click to option
+  for (let i = 0; i < selectSingle_labels.length; i++) {
+    selectSingle_labels[i].addEventListener('click', (evt) => {
+      selectSingle_title.textContent = evt.target.textContent;
+      selectSingle.setAttribute('data-state', '');
+    });
+  }
+  
+  
+  // Reset title
+  const reset = document.querySelector('.reset');
+  reset.addEventListener('click', () => {
+    selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
+  });
+}
+
+findClick();
 
 
